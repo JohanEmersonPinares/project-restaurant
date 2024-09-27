632ed9ef575eda29caaafe21d73e9d3873450fa8
Johanirus <johan_kyurem@hotmail.com>
2024-09-27 00:38:15 -0500

    feat:logo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>ORDENA TU PEDIDO YA!!</h2>
+        <p>Pide tu mejor comida elige tu favorita </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
+
